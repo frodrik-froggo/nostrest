de79f66b736edf1726c71195186f7259b49cd27c
SKP <skp@skp.rocks>
2022-12-19 21:31:12 +0100

    added some gfx

---
+++ plant/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+#plantuml -v -tsvg -o .. overview_mint.puml
+#plantuml -v -tsvg -o .. overview_mint_ideal.puml
+#plantuml -v -tsvg -o .. overview_p2p_ideal.puml
+
+plantuml -v -tpng -o .. overview_mint.puml
+plantuml -v -tpng -o .. overview_mint_ideal.puml
+plantuml -v -tpng -o .. overview_p2p_ideal.puml
